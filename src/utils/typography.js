8701197a2c506d57d14659e142f8bc82ad796ece
callumboaden <boaden.callum@gmail.com>
2021-02-22 21:55:37 +0700

    install typography.js

---
+++ src/utils/typography.js
@@ -0,0 +1,10 @@
+import Typography from "typography"
+
+const typography = new Typography({
+  baseFontSize: "16px",
+  baseLineHeight: 1.666,
+  headerFontFamily: ["Poppins", "Helvetica Neue", "sans-serif"],
+  bodyFontFamily: ["Roboto", "Helvetica Neue", "sans-serif"],
+})
+
+export default typography
